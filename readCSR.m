7d2b0f015cf47c996e8f0753d4ceb95840ff4e11
Hance Wu <32484940+wzh4464@users.noreply.github.com>
2023-08-06 12:12:43 +0800

    new read

--- readCSR.m
+++ readCSR.m
@@ -6,13 +6,49 @@ function S = readCSR(data, indptr, indices, shape)
 % shape is the shape of the matrix
 % S is the sparse matrix: OUTPUT
 
-IJ = [];
-for i = 1:length(indptr)-1
-    for j = indptr(i):indptr(i+1)-1
-        IJ = [IJ; i, indices(j)];
+data_ptr = 1;
+IJ = zeros(length(data),2);
+for i = 1:shape(1)-1
+    for j = indptr(i):indptr(i+1)+1
+        IJ(data_ptr,:) = [i, indices(j)];
+        data_ptr = data_ptr + 1;
     end
 end
 
-S = sparse(IJ(:,1), IJ(:,2), data, shape(1), shape(2));
+% numWorkers = 8;
+% parpool(numWorkers);
+
+% spmd
+%     % Get the number of workers and the ID of this worker
+%     workerID = spmdIndex;
+
+%     % Compute the indices of the rows to process on this worker
+%     rowsPerWorker = ceil(length(indptr) / numWorkers);
+%     startRow = (workerID - 1) * rowsPerWorker + 1;
+%     endRow = min(workerID * rowsPerWorker, length(indptr));
+
+%     % Process the rows on this worker
+%     tempIJ = cell(endRow - startRow + 1, 1);
+
+%     for i = startRow:endRow
+%         temp = zeros(indptr(i + 1) - indptr(i), 2);
+%         k = 1;
+
+%         for j = indptr(i):indptr(i + 1) - 1
+%             temp(k, :) = [i, indices(j)];
+%             k = k + 1;
+%         end
+
+%         tempIJ{i - startRow + 1} = temp;
+%     end
+
+%     % Combine the results from all workers
+%     IJ = cat(1, tempIJ{:});
+%     IJ = spmdCat(IJ, 1, 1);
+% end
+
+% IJ = IJ{1};
+
+S = sparse(IJ(:,1), IJ(:,2), data, shape(1), shape(2), length(data));
 
 end
