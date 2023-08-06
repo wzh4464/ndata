637395d2f449c43a70b4cc8c3adb5dbb56e46806
Hance Wu <32484940+wzh4464@users.noreply.github.com>
2023-08-07 00:23:23 +0800

    night

---
+++ curlUpload.sh
@@ -0,0 +1,11 @@
+#bash
+
+# curl to upload
+# https://dav.jianguoyun.com/dav/dataset
+# account: `zihanwu7@icloud.com`
+# password: `agv5ea83j27scqr3`
+
+# upload file `main.py` to `dataset` folder
+# curl -T main.py -u zihanwu7@icloud.com:agv5ea83j27scqr3 https://dav.jianguoyun.com/dav/dataset/main.py
+
+curl -T A.mat -u zihanwu7@icloud.com:agv5ea83j27scqr3 https://dav.jianguoyun.com/dav/dataset/main.py
---
+++ partitionA.m
@@ -0,0 +1,4 @@
+% PARTITION
+
+%% load data
+load('A.mat')
