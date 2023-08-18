d2af70e2ed7cfd9b63e3a3ae087484081c6c7f22
wzh4464_hpc <32484940+wzh4464@users.noreply.github.com>
2023-08-19 02:00:51 +0800

    run on hpc

--- run.sh
+++ run.sh
@@ -4,20 +4,21 @@
 #SBATCH --ntasks=1               # 1 task per node
 #SBATCH --mem=40GB               # Request 30GB memory
 #SBATCH --time=0-01:00:00        # Time limit day-hrs:min:sec
-#SBATCH --output=run.out         # Standard output 
 #SBATCH --error=run.err          # Error log
+#SBATCH --mail-type=ALL          # Email user when job starts, ends and aborts
+#SBATCH --mail-user=zihanwu7-c@my.cityu.edu.hk       # Email user
 
 
-# matlab to run `indices_script.m`
+# matlab to run `cocluster.m`
 
 # Path: /cm/shared/apps/maths/matlab/R2020b/bin/matlab
 # OUTPUT=/home/zihanwu7/scratch/ndata/output.txt
-OUTPUT=/home/jeff/zihan/ndata/output.txt
+OUTPUT=/home/jeff/zihan/ndata/cocluster_output.txt
 
-# cd /home/zihanwu7/scratch/ndata/
-# /cm/shared/apps/maths/matlab/R2020b/bin/matlab -nodisplay -nosplash -nodesktop -r "run('indices_script.m');exit;" >> $OUTPUT
-cd /home/jeff/zihan/ndata/
-/home/jeff/zihan/MATLAB/bin/matlab -nodisplay -nosplash -nodesktop -r "run('indices_script.m');exit;" >> $OUTPUT
+cd /home/zihanwu7/scratch/ndata/
+/cm/shared/apps/maths/matlab/R2020b/bin/matlab -nodisplay -nosplash -nodesktop -r "run('cocluster.m');exit;" >> $OUTPUT
+# cd /home/jeff/zihan/ndata/
+# /home/jeff/zihan/MATLAB/bin/matlab -nodisplay -nosplash -nodesktop -r "run('cocluster.m');exit;" >> $OUTPUT
 
 date >> $OUTPUT
 
