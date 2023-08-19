9d33830840f926cdf4fb335212f7dfdf4556eba1
wzh4464_hpc <32484940+wzh4464@users.noreply.github.com>
2023-08-19 09:45:20 +0800

    hpc result

--- run.sh
+++ run.sh
@@ -5,6 +5,7 @@
 #SBATCH --mem=40GB               # Request 30GB memory
 #SBATCH --time=0-01:00:00        # Time limit day-hrs:min:sec
 #SBATCH --error=run.err          # Error log
+#SBATCH --output=run.out         # Output log
 #SBATCH --mail-type=ALL          # Email user when job starts, ends and aborts
 #SBATCH --mail-user=zihanwu7-c@my.cityu.edu.hk       # Email user
 
@@ -16,9 +17,9 @@
 OUTPUT=/home/jeff/zihan/ndata/cocluster_output.txt
 
 cd /home/zihanwu7/scratch/ndata/
-/cm/shared/apps/maths/matlab/R2020b/bin/matlab -nodisplay -nosplash -nodesktop -r "run('cocluster.m');exit;" >> $OUTPUT
+/cm/shared/apps/maths/matlab/R2020b/bin/matlab -nodisplay -nosplash -nodesktop -r "run('cocluster.m');exit;"
 # cd /home/jeff/zihan/ndata/
 # /home/jeff/zihan/MATLAB/bin/matlab -nodisplay -nosplash -nodesktop -r "run('cocluster.m');exit;" >> $OUTPUT
 
-date >> $OUTPUT
+date
 
