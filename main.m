c5fe0a5a9c634915d2bf82dbd353e1fd0e93ebd0
wzh4464_hpc <32484940+wzh4464@users.noreply.github.com>
2023-08-23 00:27:47 +0800

    hpc change

--- main.m
+++ main.m
@@ -3,14 +3,14 @@
 %Created Date: Saturday August 19th 2023
 %Author: Hance Ng
 %-----
-%Last Modified: Tuesday, 22nd August 2023 9:29:33 pm
+%Last Modified: Wednesday, 23rd August 2023 12:26:48 am
 %Modified By: the developer formerly known as Hance Ng at <wzh4464@gmail.com>
 %-----
 %HISTORY:
 %Date      		By	Comments
 %----------		---	---------------------------------------------------------
 %
-%19-08-232023	M1  change name	
+%19-08-232023	M1  change name
 %
 %19-08-232023	HPC disp('S = ' + string(diag(S(1:20, 1:20))))
 %}
@@ -18,19 +18,25 @@
 %% Load the matfile
 mf = matfile('smallA.mat');
 [m, n] = size(mf, 'smallA');
-% load first 1000 lines of smallA without loading the whole file to memory
-A = mf.smallA(1:10000, :);
+% load first 10000 lines of smallA without loading the whole file to memory
+batch_size = 10000;
+A = mf.smallA(1:batch_size, :);
 
 %% ignore columns with all zeros and build the column map
 col_map = (sum(A, 1) ~= 0);
 A = A(:, col_map);
 
-[U,S,V] = svdsketch(A);
+tic;
+[U, S, V] = svdsketch(A);
+toc;
 
 s = diag(S);
 stem(s(1:1000));
 title('Singular Values');
-saveas(gcf, 'co-cluster_batch_size_' + string(batch_size) + '_k_' + string(k) + '_singular_values.png')
+saveas(gcf, 'co-cluster_batch_size_' + string(batch_size)  + '_singular_values.png')
 
 % save s to txt
-writematrix(s(1:1000), 'co-cluster_batch_size_' + string(batch_size) + '_k_' + string(k) + '_singular_values.txt', 'Delimiter', ' ')
+writematrix(s(1:1000), 'co-cluster_batch_size_' + string(batch_size)  + '_singular_values.txt', 'Delimiter', ' ')
+
+% save size(A) to txt
+writematrix(size(A), 'co-cluster_batch_size_' + string(batch_size)  + '_size_A.txt', 'Delimiter', ' ')
--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --partition=tiny         # Partition (job queue)
+#SBATCH --partition=stingy         # Partition (job queue)
 #SBATCH --nodes=1                # 1 computer nodes
 #SBATCH --ntasks=1               # 1 task per node
 #SBATCH --mem=40GB               # Request 30GB memory
-#SBATCH --time=0-01:00:00        # Time limit day-hrs:min:sec
+#SBATCH --time=0-06:00:00        # Time limit day-hrs:min:sec
 #SBATCH --error=run.err          # Error log
 #SBATCH --output=run.out         # Output log
 #SBATCH --mail-type=ALL          # Email user when job starts, ends and aborts
@@ -17,7 +17,7 @@ OUTPUT=/home/zihanwu7/scratch/cocluster_output.txt
 # OUTPUT=/home/jeff/zihan/ndata/cocluster_output.txt
 
 cd /home/zihanwu7/scratch/ndata/
-/cm/shared/apps/maths/matlab/R2020b/bin/matlab -nosplash -nodesktop -r "run('cocluster.m');exit;" > $OUTPUT
+/cm/shared/apps/maths/matlab/R2020b/bin/matlab -nosplash -nodesktop -r "run('main.m');exit;" > $OUTPUT
 # cd /home/jeff/zihan/ndata/
 # /home/jeff/zihan/MATLAB/bin/matlab -nodisplay -nosplash -nodesktop -r "run('cocluster.m');exit;" >> $OUTPUT
 
