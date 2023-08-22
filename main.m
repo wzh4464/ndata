7243fda0bb560e02c7e685b386b34fa340806ff5
Hance_Wu_M1 <32484940+wzh4464@users.noreply.github.com>
2023-08-22 21:29:46 +0800

    stem for s

--- main.m
+++ main.m
@@ -3,7 +3,7 @@
 %Created Date: Saturday August 19th 2023
 %Author: Hance Ng
 %-----
-%Last Modified: Tuesday, 22nd August 2023 9:12:47 pm
+%Last Modified: Tuesday, 22nd August 2023 9:29:33 pm
 %Modified By: the developer formerly known as Hance Ng at <wzh4464@gmail.com>
 %-----
 %HISTORY:
@@ -27,37 +27,10 @@ A = A(:, col_map);
 
 [U,S,V] = svdsketch(A);
 
+s = diag(S);
+stem(s(1:1000));
+title('Singular Values');
+saveas(gcf, 'co-cluster_batch_size_' + string(batch_size) + '_k_' + string(k) + '_singular_values.png')
 
-
-%% 
-% [batch_size, n] = size(A);
-% batch_size = size(A, 1);
-
-% batch_size = 10000;
-% disp('batch_size =' + string(batch_size))
-
-%% shrink A to make n divisible by batch_size
-% n = floor(n/batch_size)*batch_size;
-% A = A(:, 1:n);
-
-%%
-% M = m / batch_size;
-% N = n / batch_size;
-
-% %%
-% k = batch_size;
-
-% %% 
-% [U, S, V] = svds(A, k);
-% disp('k = ' + string(k))
-% % disp first 20 S
-% disp('S = ' + string(diag(S(1:20, 1:20))))
-
-% title('Singular Values');
-% plot(diag(S(1:500, 1:500)), 'r.');
-% saveas(gcf, 'co-cluster_batch_size_' + string(batch_size) + '_k_' + string(k) + '_singular_values.png')
-
-% % save s to txt
-% writematrix(diag(S(1:500, 1:500)), 'co-cluster_batch_size_' + string(batch_size) + '_k_' + string(k) + '_singular_values.txt', 'Delimiter', ' ')
-
-%%
+% save s to txt
+writematrix(s(1:1000), 'co-cluster_batch_size_' + string(batch_size) + '_k_' + string(k) + '_singular_values.txt', 'Delimiter', ' ')
