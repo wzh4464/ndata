1ad3a0c1ab64841e07d91731807df208bc7a5065
wzh4464_hpc <32484940+wzh4464@users.noreply.github.com>
2023-08-23 22:41:31 +0800

    revert score

--- score.m
+++ score.m
@@ -1,21 +1,31 @@
+%{
+%File: /score.m
+%Created Date: 2023-08-23 Wed
+%Author: Hance Ng
+%-----
+%Last Modified: Wednesday, 23rd August 2023 10:41:08 pm
+%Modified By: the developer formerly known as Hance Ng at <wzh4464@gmail.com>
+%-----
+%HISTORY:
+%Date      		By	Comments
+%----------		---	---------------------------------------------------------
+%}
+
 function result = score(X, I, J)
     % SCORE Compute the compatibility for submatrix X_IJ
     %
     %   X: the data matrix
     %   I: the row cluster assignment; I is logical vector.
     %   J: the column cluster assignment; J is logical vector.
-    
+
     % I = logical(I);
     % J = logical(J);
-    numericI = find(I);
-    numericJ = find(J);
-    % subset = X(numericI, numericJ);
 
     lenI = sum(I);
     lenJ = sum(J);
 
-    S1 = abs(corrcoef(X(numericI, numericJ)) - eye(lenJ));
-    S2 = abs(corrcoef(X(numericI, numericJ)') - eye(lenI));
+    S1 = abs(corrcoef(X(I, J)) - eye(lenJ));
+    S2 = abs(corrcoef(X(I, J)') - eye(lenI));
 
     result = min([scoreHelper(lenJ, S1), scoreHelper(lenI, S2)]);
 end
